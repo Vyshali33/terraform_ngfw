af9af35e0e4e3e8cc801adc374ab6daf6211c864
vyshali <vyshali@gmail.com>
2024-10-22 03:05:55 -0400

    modified outputs

--- modules/vpc_module/outputs.tf
+++ modules/vpc_module/outputs.tf
@@ -2,11 +2,11 @@ output "vpc_network" {
   value = google_compute_network.vpc_network
 }
 output "self_link" {
-  value = google_compute_network.self_link
+  value = google_compute_network.vpc_network.self_link
 }
 output "numeric_id" {
-  value = google_compute_network.numeric_id
+  value = google_compute_network.vpc_network.numeric_id
 }
 output "gateway_ipv4" {
-  value = google_compute_network.gateway_ipv4
+  value = google_compute_network.vpc_network.gateway_ipv4
 }
